3b6edbe810865998397d5090aad969f1292006b4
Scott Warner <Tortel1210@gmail.com>
2018-02-19 16:05:07 -0500

    Use pico gapps for arm, build Exchange2

--- product.mk
+++ product.mk
@@ -1,11 +1,19 @@
 #PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
-PRODUCT_PACKAGES += auditd
+PRODUCT_PACKAGES += \
+    auditd \
+    Exchange2
 
 #PRODUCT_PROPERTY_OVERRIDES += \
 #    lineage.updater.uri=http://updater.theflamingskull.com/api
 
 #$(call prepend-product-if-exists, vendor/tortel/product.mk)
 
-GAPPS_VARIANT := nano
+ifneq ($(filter arm64,$(TARGET_ARCH)),)
+  GAPPS_VARIANT := nano
+else
+  GAPPS_VARIANT := pico
+endif
+
+
 $(call inherit-product-if-exists, vendor/opengapps/build/opengapps-packages.mk)
 
