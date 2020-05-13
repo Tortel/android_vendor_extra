bbf9aff3f34b7e79ac3f59652c8049ace4753228
Scott Warner <Tortel1210@gmail.com>
2020-05-13 14:15:50 -0400

    Update fingerprint

--- build/core/config.mk
+++ build/core/config.mk
@@ -9,7 +9,7 @@ PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 4 XL
 PRODUCT_MANUFACTURER := Google
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="coral" \
     PRODUCT_BRAND="Google" \
@@ -17,10 +17,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="coral" \
     PRODUCT_MODEL="Pixel 4 XL" \
     PRODUCT_MANUFACTURER="Google" \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ2A.200405.005 6254899 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ2A.200501.001.B2 6352890 release-keys"
 
 # Fix the security patch to keep in line with fingerprint
-PLATFORM_SECURITY_PATCH := 2020-04-05
+PLATFORM_SECURITY_PATCH := 2020-05-05
 
 endif
 
