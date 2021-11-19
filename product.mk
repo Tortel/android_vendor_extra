f7b52f9df912fb68a1e1756d9fc16b94867a6e35
Scott Warner <Tortel1210@gmail.com>
2021-11-18 21:11:44 -0500

    Clean up all the random crap
    The OP camera/gallery don't really work anymore - just trash all of it

--- product.mk
+++ product.mk
@@ -6,18 +6,14 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     auditd
 
+# OpenGapps related section
 GAPPS_EXCLUDED_PACKAGES := GooglePackageInstaller
 
 GAPPS_PRODUCT_PACKAGES += \
     AndroidAuto
 
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    lineage.updater.uri=http://updater.theflamingskull.com/api
-
-#$(call prepend-product-if-exists, vendor/tortel/product.mk)
-
 GAPPS_VARIANT := nano
-#$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+#$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 
 # Sounds
 PRODUCT_COPY_FILES += \
@@ -30,33 +26,6 @@ PRODUCT_COPY_FILES += \
     vendor/extra/adb_keys:$(TARGET_RECOVERY_ROOT_OUT)/root/adb_keys \
     vendor/extra/adb_keys:$(TARGET_ROOT_OUT)/adb_keys
 
-# OnePlus 7 specific stuff
-ifneq ($(filter lineage_guacamole,$(TARGET_PRODUCT)),)
-# OnePlus camera-related apps
-PRODUCT_PACKAGES += \
-#    OPCameraResources \
-#    OnePlusCamera \
-#    OnePlusCameraService \
-    OnePlusGallery
-
-# OnePlus mock crap
-PRODUCT_PACKAGES += oneplus-mock
-PRODUCT_BOOT_JARS += oneplus-mock
-
-# Permissions for OnePlus camera
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/op/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
-    $(LOCAL_PATH)/op/etc/sysconfig/hiddenapi-package.whitelist-oneplus.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hiddenapi-package.whitelist-oneplus.xml
-
-# Extra camera libs
-PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/op/lib/libarcsoft_portrait_distortion_correction.so:$(TARGET_COPY_OUT_VENDOR)/lib/libarcsoft_portrait_distortion_correction.so \
-   $(LOCAL_PATH)/op/lib64/libarcsoft_portrait_distortion_correction.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libarcsoft_portrait_distortion_correction.so \
-   $(LOCAL_PATH)/op/lib64/libDxHdcp.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libDxHdcp.so
-
-endif
-
-#$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
-
 # Gapps
 $(call inherit-product-if-exists, vendor/gapps/common/common-vendor.mk)
+WITH_GMS := true
