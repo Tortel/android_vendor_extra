415ac883832a1c6e3dc45949f73bbb3dba8057a8
Scott Warner <Tortel1210@gmail.com>
2021-11-19 15:30:59 -0500

    Turn x2 into a Pixel XL

--- build/core/config.mk
+++ build/core/config.mk
@@ -24,3 +24,27 @@ PLATFORM_SECURITY_PATCH := 2021-09-05
 
 endif
 
+ifeq ($(PRODUCT_NAME),lineage_x2)
+
+TARGET_OTA_ASSERT_DEVICE := marlin
+
+# Fake being a Pixel XL
+#PRODUCT_DEVICE := marlin
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Pixel XL
+PRODUCT_MANUFACTURER := Google
+
+BUILD_FINGERPRINT := "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="marlin" \
+    PRODUCT_BRAND="Google" \
+    TARGET_DEVICE="marlin" \
+    PRODUCT_DEVICE="marlin" \
+    PRODUCT_MODEL="Pixel XL" \
+    PRODUCT_MANUFACTURER="Google" \
+    PRIVATE_BUILD_DESC="marlin-user 10 QP1A.191005.007.A3 5972272 release-keys"
+
+# Fix the security patch to keep in line with fingerprint
+PLATFORM_SECURITY_PATCH := 2019-10-06
+
+endif
